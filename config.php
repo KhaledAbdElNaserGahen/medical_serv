d77db33474b8d0bf3fa9c368864b0e00329bda6d
KhaledAbdElNaserGahen <113264617+KhaledAbdElNaserGahen@users.noreply.github.com>
2023-09-14 02:16:29 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+session_start();
+
+// base file link 
+define("BL",__DIR__.'/');
+define("BLA",__DIR__.'/admin/');
+
+// base url
+define("BU","http://127.0.0.1/medical_serv/");
+define("BUA","http://127.0.0.1/medical_serv/admin/");
+define("ASSETS","http://127.0.0.1/medical_serv/assets/");
+
+require BL.'functions/db.php'; 
+?>
+
